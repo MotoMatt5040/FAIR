848cfa691843ef660de5753b5179af1e1c9fc345
MotoMatt5040 <104686636+MotoMatt5040@users.noreply.github.com>
2022-05-10 15:46:40 -0700

    Logger for error handling initialized

--- main.py
+++ main.py
@@ -2,5 +2,22 @@
 # V 1.0
 # 5/10/2022
 
+import logging
+import yfinance as yf
+import pandas as pd
+import numpy as np
+from datetime import date, timedelta, datetime
+
+logging.basicConfig(level=logging.DEBUG)
+logging.root.setLevel(logging.DEBUG)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(funcName)s:%(levelName)s:%(message)s')
+file_handler = logging.FileHandler('logs\main.log')
+file_handler.setFormatter(formatter)
+logger.addHandler(file_handler)
+logger.propegate = False
+
 if __name__ == '__main__':
     pass
