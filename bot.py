71d4ab903a8f78c904a7c6b739d11ff3bd18e124
MotoMatt5040 <104686636+MotoMatt5040@users.noreply.github.com>
2022-05-25 17:38:49 -0500

    Major bot changes. This will likely be the new platform.

--- bot.py
+++ bot.py
@@ -1,18 +1,34 @@
-import logging
-
+import threading
 import MetaTrader5 as mt5
-import Candle, threading, Orders
+import tick_reader, slope_abs_rel, orders
+import MACD, RSI
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(funcName)s:%(message)s')
-file_handler = logging.FileHandler('logs\\bot.log')
-file_handler.setFormatter(formatter)
-logger.addHandler(file_handler)
-logger.propagate = False
 
 class Bot:
+    # Attributes
+    threads = []
+    ticks = []
+    pill2kill = threading.Event()
 
+    trading_data = {
+        "lotage": 0.0,
+        "time_period": 0,
+        "avg_spread": -1,
+        "market": "",
+        "buy_model": None,
+        "sell_Model": None
+    }
+    indicators = {
+        "MACD": {"MACD": 0.0, "SIGNAL": 0.0},
+        "RSI": 0.0,
+        "slope": 0.0,
+        "absolute_max": {"time": 0.0, "difference": 0.0},
+        "absolute_min": {"time": 0.0, "difference": 0.0},
+        "relative_min": {"time": 0.0, "difference": 0.0},
+        "relative_max": {"time": 0.0, "difference": 0.0}
+    }
+
+    # Methods
     def __init__(self, lotage: float, time_period: int, market: str):
         """Constructor of the bot. It justs fills the needed informartion for the bot.
 
@@ -21,54 +37,96 @@ class Bot:
             time_period (int): Time period of the bot, 1 minute, 15 minutes... (in seconds)
             market (str): Market to operate in.
         """
-        logger.info(
-            f'''Initializing Bot with the following parameters:
-    Lotage: {lotage}
-    Time Period: {time_period}
-    Market: {market}'''
-        )
-        self.threads = []
-        self.data = {'0': None, '1': None}
-        self.pill2kill = threading.Event()
-        self.trading_data = {}
         self.trading_data['lotage'] = lotage
         self.trading_data['time_period'] = time_period
         self.trading_data['market'] = market
+        self.trading_data['avg_spread'] = 0
 
-    def thread_candles(self):
-        """Function to launch the data thread."""
-        logger.info('Creating threads for Candles')
-        t = threading.Thread(target=Candle.thread_candle,
-                             args=(self.pill2kill, self.data))
+    def get_ticks(self) -> list:
+        """Method to get the ticks.
+
+        Returns:
+            list: List of ticks
+        """
+        return self.ticks
+
+    def thread_tick_reader(self):
+        """Function to launch the tick reader thread.
+        """
+        t = threading.Thread(target=tick_reader.thread_tick_reader,
+                             args=(self.pill2kill, self.ticks, self.trading_data,))
         self.threads.append(t)
         t.start()
-        logger.info(f'''Threads started with the following parameters
-Thread: {t}''')
+        print('Thread - tick_reader. LAUNCHED')
+
+    def thread_slope_abs_rel(self):
+        """Function to launch the thread for calculating the slope
+        and, the absolute and relative points in the chart.
+        """
+        t = threading.Thread(target=slope_abs_rel.thread_slope_abs_rel,
+                             args=(self.pill2kill, self.ticks, self.indicators))
+        self.threads.append(t)
+        t.start()
+        print('Thread - slope_abs_rel. LAUNCHED')
+
+    def thread_MACD(self):
+        """Function to launch the thread for calculating the MACD.
+        """
+        t = threading.Thread(target=MACD.thread_macd,
+                             args=(self.pill2kill, self.ticks, self.indicators, self.trading_data))
+        self.threads.append(t)
+        t.start()
+        print('Thread - MACD. LAUNCHED')
+
+    def thread_RSI(self):
+        t = threading.Thread(target=RSI.thread_RSI,
+                             args=(self.pill2kill, self.ticks, self.indicators))
+        self.threads.append(t)
+        t.start()
+        print('Thread - RSI. LAUNCHED')
 
     def thread_orders(self):
-        """Function to launch the thread for sending orders."""
-        t = threading.Thread(target=Orders.thread_orders,
-                             args=(self.pill2kill, self.data, self.trading_data))
+        t = threading.Thread(target=orders.thread_orders,
+                             args=(self.pill2kill, self.trading_data))
         self.threads.append(t)
         t.start()
-        print('Thread - ORDERS. LAUNCHED')
+        print('Thread - orders. LAUNCHED')
+
+    def mt5_login(self, usr: int, password: str) -> bool:
+        """Function to initialize the metatrader 5 aplication
+        and login wiht our account details.
+
+        Args:
+            usr (int): User ID.
+            password (str): Password
+
+        Returns:
+            bool: True if everything is OK, False if not
+        """
+        # Initialize mt5 (if not already)
+        if not mt5.initialize():
+            print("initialize() failed, error code =", mt5.last_error())
+            return False
+
+        # Login into mt5
+        authorized = mt5.login(usr, password, server='MetaQuotes-Demo')
+        if not authorized:
+            print("failed to connect at account #{}, error code: {}".format(usr, mt5.last_error()))
+            return False
+        return True
 
     def kill_threads(self):
-        """Function to kill all the loaded threads."""
+        """Function to kill all the loaded threads.
+        """
         print('Threads - Stopping threads')
         self.pill2kill.set()
         for thread in self.threads:
             thread.join()
 
-    def start(self):
-        """Function to start all the threads"""
-        logger.info('Starting bot...')
-        self.thread_candles()
-        self.thread_orders()
-
     def wait(self):
-        """Function to make the thread wait."""
-        # Input para detener a los hilos
+        """Function to make the thread wait.
+        """
+        # Input to stop the threads
         print('\nPress ENTER to stop the bot\n')
         input()
         self.kill_threads()
--- main.py
+++ main.py
@@ -20,56 +20,92 @@ file_handler.setFormatter(formatter)
 logger.addHandler(file_handler)
 logger.propagate = False
 
-usr = None
-password = None
-server = None
+# usr = None
+# password = None
+# server = None
+#
+# while usr is None:
+#     try:
+#         usr = int(input('Username: '))
+#     except:
+#         print('Incorrect user.')
+# password = input('Password: ')
+# print('''Which server would you like to connect to?
+#     1: MetaQuotes-Demo''')
+# while server is None:
+#     if keyboard.is_pressed('1'):
+#         server = 'MetaQuotes-Demo'
+#         print(f'\nYou selected: {server}\n')
+#
+# logger.info('Opening/Initializing MT5 Connection')
+# usr = 5003534489
+# password = '6jsxifbk'
+# if mt5.initialize(login=usr, password=password, server='MetaQuotes-Demo'):
+#     print(f'Connection to MetaTrader5 server successful.')
+#     print()
+#     account_info = mt5.account_info()
+#     if account_info is not None:
+#         account_info_dict = mt5.account_info()._asdict()
+#         print('*****Account Info*****\n')
+#         for item in account_info_dict:
+#             print(f"{item}: {account_info_dict[item]}")
+#         print()
+#         print()
+#     terminal_info = mt5.terminal_info()
+#     if terminal_info is not None:
+#         print('*****Terminal Info*****\n')
+#         terminal_info_dict = mt5.terminal_info()._asdict()
+#         for item in terminal_info_dict:
+#             print(f"{item}: {terminal_info_dict[item]}")
+#         print()
+#         print()
+# else:
+#     quit(f'''Connection to MetaTrader5 server unsuccessful!
+#     Error: {mt5.last_error()}
+#
+#     Please restart the program.
+# ''')
+#
+# macd_bot = Bot(lotage=0.1, time_period=60, market="Boom 300 Index")
+#
+# macd_bot.start()
+# macd_bot.wait()
 
-while usr is None:
-    try:
-        usr = int(input('Username: '))
-    except:
-        print('Incorrect user.')
-password = input('Password: ')
-print('''Which server would you like to connect to?
-    1: MetaQuotes-Demo''')
-while server is None:
-    if keyboard.is_pressed('1'):
-        server = 'MetaQuotes-Demo'
-        print(f'\nYou selected: {server}\n')
+import bot
+import matplotlib.pyplot as plt
 
-logger.info('Opening/Initializing MT5 Connection')
-usr = 5003534489
-password = '6jsxifbk'
-if mt5.initialize(login=usr, password=password, server='MetaQuotes-Demo'):
-    print(f'Connection to MetaTrader5 server successful.')
-    print()
-    account_info = mt5.account_info()
-    if account_info is not None:
-        account_info_dict = mt5.account_info()._asdict()
-        print('*****Account Info*****\n')
-        for item in account_info_dict:
-            print(f"{item}: {account_info_dict[item]}")
-        print()
-        print()
-    terminal_info = mt5.terminal_info()
-    if terminal_info is not None:
-        print('*****Terminal Info*****\n')
-        terminal_info_dict = mt5.terminal_info()._asdict()
-        for item in terminal_info_dict:
-            print(f"{item}: {terminal_info_dict[item]}")
-        print()
-        print()
-else:
-    quit(f'''Connection to MetaTrader5 server unsuccessful!
-    Error: {mt5.last_error()}
-    
-    Please restart the program.
-''')
+# Creating a bot
+b = bot.Bot(0.01, 15*60, "EURUSD")
 
-macd_bot = Bot(lotage=0.1, time_period=60, market="Boom 300 Index")
+with open("login_data.txt", 'r') as f:
+    lines = f.readlines()
+    usr = int(lines[0])
+    password = lines[1]
 
-macd_bot.start()
-macd_bot.wait()
+
+# Login into mt5
+if not b.mt5_login(usr, password):
+    quit()
+b.thread_tick_reader()
+b.thread_slope_abs_rel()
+b.thread_MACD()
+b.thread_RSI()
+b.thread_orders()
+b.wait()
+
+# Making a graph of the data
+second_list = b.get_ticks()
+xAxis = []
+yAxis = []
+i = 1
+if len(second_list) < 10000:
+    for element in b.get_ticks():
+        xAxis.append(i)
+        yAxis.append(element)
+        i += 1
+
+plt.plot(xAxis, yAxis)
+plt.show()
 
 
 if __name__ == '__main__':
