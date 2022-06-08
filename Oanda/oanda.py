29eae187c7c7e6130922627da276918c20dfe890
MotoMatt5040 <104686636+MotoMatt5040@users.noreply.github.com>
2022-06-08 14:21:02 -0500

    oanda

--- Oanda/oanda.py
+++ Oanda/oanda.py
@@ -1,7 +1,44 @@
 import pandas as pd
-import tpqoa
+import numpy as np
+from datetime import timedelta, date
+# import tpqoa
+import yfinance as yf
+import matplotlib.pyplot as plt
 
-api = tpqoa.tpqoa("oanda.cfg")
+plt.style.use('seaborn')
+
+# api = tpqoa.tpqoa("oanda.cfg")
+
+ticker = ["AAPL", "BA", "KO", 'IBM', "DIS", 'MSFT']
+stocks = yf.download(tickers=ticker, start=date.today() - timedelta(365), end=date.today())
+# print(stocks.to_string())
+stocks.to_csv('stocks.csv')
+stocks = pd.read_csv('stocks.csv', header=[0, 1], index_col=[0], parse_dates=[0])
+
+# stocks = stocks.swaplevel(axis=1).sort_index(axis=1)
+print(stocks)
+
+close = stocks.loc[:, 'Close'].copy()
+print(close)
+
+ret = close.pct_change().dropna()
+print(ret)
+summary = ret.describe().T
+print(summary.to_string()) #  T Transposes the list
+
+summary['mean'] = summary['mean']*252
+summary['std'] = summary['std']* np.sqrt(252)
+
+print(summary.to_string()) #  T Transposes the list
+
+
+
+
+# norm = close.div(close.iloc[0]).mul(100)
+# norm.plot(figsize=(15, 8), fontsize=13)
+# close.plot(figsize=(15, 8), fontsize=13)
+# plt.legend(fontsize=13)
+# plt.show()
 
 # summary = api.get_account_summary()
 # for item in summary:
