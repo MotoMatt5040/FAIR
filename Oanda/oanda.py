5d43b5ff78413e0ef2f08167a2e6bf8c39808881
MotoMatt5040 <104686636+MotoMatt5040@users.noreply.github.com>
2022-07-11 13:59:29 -0500

    Timeframe adjusted to 50 minutes

--- Oanda/oanda.py
+++ Oanda/oanda.py
@@ -24,14 +24,14 @@ print(sma_updater, sma_updater[0], sma_updater[1])
 print(str(datetime.now() - timedelta(hours=1))[:-7])
 print(datetime.now())
 
-smatrader = st.SMACTrader("oanda.cfg", "EUR_USD", "5min", smas=sma_updater[0], smal=sma_updater[1], units=500000)
+smatrader = st.SMACTrader("oanda.cfg", "EUR_USD", "15min", smas=sma_updater[0], smal=sma_updater[1], units=500000)
 # smau = smabt.SMABacktester(instrument='EUR_USD', start=str(date.today() - timedelta(1)), end=str(date.today()), granularity='M1', price='B', SMA_S=37, SMA_L=1)
-smau = smabt.SMABacktester(instrument='EUR_USD', start=str(datetime.now() - timedelta(days=1))[:-7],
-                           end=str(datetime.now())[:-7], granularity='M5', price='B', SMA_S=37, SMA_L=1)
+smau = smabt.SMABacktester(instrument='EUR_USD', start=str(datetime.now() - timedelta(days=7))[:-7],
+                           end=str(datetime.now())[:-7], granularity='M15', price='B', SMA_S=37, SMA_L=1)
 
 
 def sma_update():
-    print(smau.optimize_parameters((1, 50, 1), (51, 170, 1))[0])
+    print(smau.optimize_parameters((1, 20, 1), (21, 100, 1))[0])
     smau.plot_results()
 
 
@@ -51,23 +51,23 @@ def sma_trading():
         #     time.sleep(900)
 
 
-print(lags, type(lags), granularity, type(granularity), sep=', ')
+# print(lags, type(lags), granularity, type(granularity), sep=', ')
 
 
-trader = mt.MLTrader("oanda.cfg", "EUR_USD", "5min", model=lm, lags=lags, granularity='S5', units=100000)
+# trader = mt.MLTrader("oanda.cfg", "EUR_USD", "15min", model=lm, lags=lags, granularity='S5', units=100000)
 
-def ml_trading():
-    while True:
-        try:
-            trader.get_most_recent()
-            trader.stream_data(trader.instrument, stop=20000)
-            if trader.position != 0:  # if we have a final open position
-                close_order = trader.create_order(trader.instrument, units=-trader.position * trader.units,
-                                                  suppress=True, ret=True)
-                trader.report_trade(close_order, "GOING NEUTRAL")
-                trader.position = 0
-        except Exception as err:
-            print(err)
+# def ml_trading():
+#     while True:
+#         try:
+#             trader.get_most_recent()
+#             trader.stream_data(trader.instrument, stop=20000)
+#             if trader.position != 0:  # if we have a final open position
+#                 close_order = trader.create_order(trader.instrument, units=-trader.position * trader.units,
+#                                                   suppress=True, ret=True)
+#                 trader.report_trade(close_order, "GOING NEUTRAL")
+#                 trader.position = 0
+#         except Exception as err:
+#             print(err)
 
 if __name__ == '__main__':
     sma_trading()
